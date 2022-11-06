3fca568630451fb07aa171d352d2c66ca4e6352f
Seokmin <newacon@naver.com>
2022-11-06 17:29:30 +0200

    toDoList localstorage update

--- index.js
+++ index.js
@@ -8,6 +8,13 @@ const loginHidden = document.querySelector(".login-hidden");
 const usernameValue = document.querySelector(".login-form__username");
 const greeting = document.querySelector(".greeting");
 //const background = document.body.background;
+const TODOS_KEY = "todos";
+let toDos = [];
+
+function saveToDos() {
+  localStorage.setItem(TODOS_KEY, JSON.stringify(toDos));
+}
+
 const HIDDEN_CLASS = "hidden";
 const USERNAME_KEY = "username";
 
@@ -41,21 +48,29 @@ function paintGreetings(username) {
 
 function deleteToDo(event) {
   const li = event.target.parentElement;
-  console.log(event.target.parentElement);
   li.remove();
+  toDos = toDos.filter((toDo) => toDo.id !== parseInt(li.id));
+  saveToDos();
 }
 
 function handleToDoBtn(event) {
   event.preventDefault();
   const newTodo = input.value;
   input.value = "";
-  toDolist(newTodo);
+  const newToDoObj = {
+    text: newTodo,
+    id: Date.now(),
+  };
+  toDos.push(newToDoObj);
+  toDolist(newToDoObj);
+  saveToDos();
 }
 
 function toDolist(newTodo) {
   const li = document.createElement("li");
+  li.id = newTodo.id;
   const span = document.createElement("span");
-  span.innerText = newTodo;
+  span.innerText = newTodo.text;
   const button = document.createElement("button");
   button.innerText = "❌";
   button.addEventListener("click", deleteToDo);
@@ -66,5 +81,14 @@ function toDolist(newTodo) {
 
 timer();
 setInterval(timer, 1000);
+
 btn.addEventListener("click", handleToDoBtn);
 loginFormLogin.addEventListener("click", handleLogin);
+
+const savedToDos = localStorage.getItem(TODOS_KEY);
+
+if (savedToDos !== null) {
+  const parsedToDos = JSON.parse(savedToDos);
+  toDos = parsedToDos;
+  parsedToDos.forEach(toDolist);
+}
